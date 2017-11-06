771da850b4915e7c0ded33ca7d066a64549fee6e
MartinBecerra19 <martinbecerra1996@gmail.com>
2017-11-06 19:59:43 +0100

    paso 2: funcion SumaArray ok

--- main.js
+++ main.js
@@ -12,6 +12,29 @@
  */
 function SumaArray(numeros) {
     let suma = 0;
-    
+    for (let a of numeros) {
+        suma = suma + a;
+    }
     return suma
 }
+// let s1 = SumaArray([2, 3, 4, 5]);
+// console.log(s1)
+function SumaArray2(numeros) {
+    let suma = 0;
+    for (let pos in numeros) {
+        suma = suma + numeros[pos];
+    }
+    return suma
+}
+// let s2 = SumaArray2([2, 3, 4, 5]);
+// console.log(s2)
+function SumaArray3(numeros) {
+    let suma = 0;
+    for (let i = 0; i < numeros.length; i++) {
+        suma = suma + numeros[i];
+    }
+
+    return suma
+}
+let s3 = SumaArray3([2, 3, 4, 5]);
+console.log(s3)
