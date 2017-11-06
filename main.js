47e9bfd7751c5584ada5fcbb4696ad287a73fe26
MartinBecerra19 <martinbecerra1996@gmail.com>
2017-11-06 19:33:46 +0100

    primer paso

---
+++ main.js
@@ -0,0 +1,17 @@
+/**
+ * @fileOverview Este archivo contiene ejemplos 
+ * de funciones con for
+ * @author Martín Becerra
+ * @version 1.0
+ */
+
+/**
+ * @description SumaArray suma los valores de un array
+ * @param {Array} numeros array de números
+ * @return {Number} 
+ */
+function SumaArray(numeros) {
+    let suma = 0;
+    
+    return suma
+}
